a323411ca9aa7682f5f3118ff2bd2f622fb67b5a
Kanishak5228 <105204873+Kanishak5228@users.noreply.github.com>
2023-05-07 21:40:21 +0530

    Update Q22.C

--- FATS/Q22.C
+++ FATS/Q22.C
@@ -1,31 +1,24 @@
 #include <stdio.h>
-#include <string.h>
 
-#define MAX_SIZE 100
-
-char* intToRoman(int num);
-
-int main() {
-    int num;
-    printf("Enter a number to convert to Roman numerals: ");
-    scanf("%d", &num);
-    char* roman = intToRoman(num);
-    printf("%d in Roman numerals is: %s\n", num, roman);
-    return 0;
+void yearToRoman(int year) {
+    int decimalValues[] = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
+    char* romanNumerals[] = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
+    char roman[1000] = "";
+    int i = 0;
+    
+    while (year > 0) {
+        while (year >= decimalValues[i]) {
+            year -= decimalValues[i];
+            strcat(roman, romanNumerals[i]);
+        }
+        i++;
+    }
+    
+    printf("%s\n", roman);
 }
 
-char* intToRoman(int num) {
-    char* roman = (char*) malloc(sizeof(char) * MAX_SIZE);
-    int value[] = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
-    char* symbol[] = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
-    int i = 0;
-    while(num > 0) {
-        if(num >= value[i]) {
-            strcat(roman, symbol[i]);
-            num -= value[i];
-        } else {
-            i++;
-        }
-    }
-    return roman;
+int main() {
+    int year = 1988;
+    yearToRoman(year);
+    return 0;
 }
