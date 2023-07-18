67b9d8313eb5e3cd0aea44a69a53b08e81265835
Kanishak5228 <105204873+Kanishak5228@users.noreply.github.com>
2023-07-19 00:48:54 +0530

    Update udp_srvr.py

--- FAT/udp_srvr.py
+++ FAT/udp_srvr.py
@@ -1,22 +1,19 @@
 import socket
 
 def start_server():
-    server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    server_socket.bind(('localhost', 12345))
-    print("Server is listening on localhost:12345")
+    host = '127.0.0.1'
+    port = 12345
+
+    server = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    server.bind((host, port))
+
+    print(f"Server listening on {host}:{port}")
 
     while True:
-        data, client_address = server_socket.recvfrom(1024)
-        data = data.decode('utf-8')
-        if not data:
-            break
+        message, client_address = server.recvfrom(1024)
+        print(f"Received from client at {client_address[0]}:{client_address[1]}: {message.decode('utf-8')}")
+        response = input("Your response: ").encode('utf-8')
+        server.sendto(response, client_address)
 
-        print("Client:", data)
-
-        response = input("Server response: ")
-        server_socket.sendto(response.encode('utf-8'), client_address)
-
-    server_socket.close()
-
-if __name__ == "__main__":
+if _name_ == "_main_":
     start_server()
